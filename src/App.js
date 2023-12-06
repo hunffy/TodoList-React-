43ceb3f45ca6d6e80d6db494f9b1ea66e14b2526
ksh <sehun910124@naver.com>
2023-12-06 13:20:08 +0900

    react로 todolist구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import plus from "./add.png";
+import { useState } from "react";
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  function add() {
+    if (inputValue === "") {
+      alert("내용을 입력하세요");
+    } else {
+      setTodos([...todos, { text: inputValue, checked: false }]);
+      setInputValue("");
+    }
+  }
+
+  function delete_list(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index] = {
+      ...newTodos[index],
+      checked: !newTodos[index].checked,
+    };
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Wrapper">
+      <header className="tit">todo_List</header>
+      <div className="content">
+        <div className="input_f">
+          <input
+            id="todo_input"
+            type="text"
+            placeholder="내용을 입력하세요"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          ></input>
+          <button id="add_btn" onClick={add}>
+            <img src={plus}></img>
+          </button>
+        </div>
+        <div className="result">
+          {todos.map((todo, index) => (
+            <div className="todolist" key={index}>
+              <span
+                onClick={() => toggleTodo(index)}
+                className={`todolist ${
+                  todos[index].checked ? "completed" : ""
+                }`}
+              >
+                {todo.text}
+              </span>
+              <button className="delete_btn" onClick={() => delete_list(index)}>
+                X
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
